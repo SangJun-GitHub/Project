bd1ffdf98763b7f0a1e6e4faa99dbb543cee4e21
SangJun-HitHub <sjpark24@myseneca.ca>
2018-09-06 16:48:44 -0400

    commit

---
+++ spring1/Main.java
@@ -0,0 +1,13 @@
+package spring1;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args){
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppContext.class);
+        Greeter g = ctx.getBean("greeter", Greeter.class);
+        String msg = g.greet("spring");
+        System.out.println(msg);
+        ctx.close();
+    }
+}
