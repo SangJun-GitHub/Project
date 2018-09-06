23c8abe2a63780bc756363a1ffba57ee31b30738
SangJun-HitHub <sjpark24@myseneca.ca>
2018-09-06 17:59:55 -0400

    commit

---
+++ spring2/Assembler.java
@@ -0,0 +1,25 @@
+package spring2;
+
+//dependency error
+
+import spring.ChangePasswordService;
+import spring.MemberDao;
+import spring.MemberRegisterService;
+
+public class Assembler {
+
+    private MemberDao memberDao;
+    private MemberRegisterService registerService;
+    private ChangePasswordService passwordService;
+
+    public Assembler(){
+        memberDao = new MemberDao();
+        registerService = new MemberRegisterService();
+        passwordService = new ChangePasswordService();
+        passwordService.setMemberDao(memberDao);
+    }
+
+    public MemberDao getMemberDao(){ return memberDao; }
+    public MemberRegisterService getMemberRegisterService(){ return registerService; }
+    public ChangePasswordService getChangePasswordService(){ return passwordService; }
+}
