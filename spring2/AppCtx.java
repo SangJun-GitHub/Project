982baf9b0d4fe84e96e4e3af4f45b48b8a63e9ab
SangJun-HitHub <sjpark24@myseneca.ca>
2018-09-11 21:47:28 -0400

    commit

---
+++ spring2/AppCtx.java
@@ -0,0 +1,35 @@
+package spring2;
+
+/**
+ * Created by bobsang89@gmail.com on 2018-09-11
+ * Project: blog
+ * Github : http://github.com/SangJun-GitHub
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import spring2.ChangePasswordService;
+import spring2.MemberDao;
+import spring2.MemberRegisterService;
+
+@Configuration
+public class AppCtx {
+
+    @Bean
+    public MemberDao memberDao(){
+        return new MemberDao();
+    }
+
+    @Bean
+    public MemberRegisterService memberRegisterService(){
+        return new MemberRegisterService(memberDao());
+    }
+
+    @Bean
+    public ChangePasswordService changePasswordService(){
+        ChangePasswordService PasswordService = new ChangePasswordService();
+        PasswordService.setMemberDao(memberDao());
+        return PasswordService;
+    }
+}
